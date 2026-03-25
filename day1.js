f2ffa2aa589ee5b9b7b43d200ab1e9d1513e2222
Deepak <deepaksaini2838@gmail.com>
2026-03-25 15:20:46 +0530

    Updated code

--- day1.js
+++ day1.js
@@ -1,5 +1,44 @@
-// Variables in the JS 
+// Variables in the JS
 
-var companyName = 'Saasaro';  // used in the past currently it is not used in production because it have some problem
-let myAge = 25;  // we used this instead of var in the production and it blocked scoped variable  { value can be change after declariation }
-const myName = 'Deepak'; //it does not change the value if we assign the value 
+var companyName = "Saasaro"; // used in the past currently it is not used in production because it have some problem
+let myAge = 25; // we used this instead of var in the production and it blocked scoped variable  { value can be change after declariation }
+const myName = "Deepak"; //it does not change the value if we assign the value
+
+// Data types
+
+//premitive Data type
+let age = 25; // Number
+let Name = "Deepak"; // string
+let isCompleted = false; // boolean
+let x = null; // null
+let y; // undefined
+let sym = Symbol("unique"); // symbol
+let bigNumber = 1234567898765432; // Bigint
+
+// Non-premitive Data type
+
+// Object
+let object = {
+  myAge: 25,
+  name: "Deepak",
+};
+
+//Array
+
+let rendomNum = [1, 2, 3, 5, 87, 23];
+
+//function
+
+function car() {
+  console.log("I buy a new Car.");
+}
+
+
+
+// Condition Statement in js
+
+if (age>18) {
+    console.log('He is a adult');
+} else {
+    console.log('Not a Adult');
+}
---
+++ task/day1task.js
@@ -0,0 +1,31 @@
+//even and odd check in js 
+
+function isEven(num) {
+
+if (num%2==0) {
+    console.log('Number is even');
+} else {
+    console.log('Number is Odd');
+}
+}
+
+isEven(32);
+
+//check number is greater and less and equal 
+function checkNumber(num){
+    if (num==0) {
+        console.log('number is Zero')
+    }else if (num<0){
+        console.log('number is less than 0')
+
+    }  
+    else{
+console.log('number is greater than 0')
+    }
+}
+checkNumber(8)
+
+
+//Largest of 3 Numbers
+
+ 
