1005fc91195dfef839e561c644f1a3d335665299
Deepak <deepaksaini2838@gmail.com>
2026-03-25 11:55:39 +0530

    Initial commit

---
+++ day1.js
@@ -0,0 +1,5 @@
+// Variables in the JS 
+
+var companyName = 'Saasaro';  // used in the past currently it is not used in production because it have some problem
+let myAge = 25;  // we used this instead of var in the production and it blocked scoped variable  { value can be change after declariation }
+const myName = 'Deepak'; //it does not change the value if we assign the value 
